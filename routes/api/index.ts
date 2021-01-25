ff47c8370eee69e4bb50fadf1f20c02ac145d5b6
Song <songyw9812@gmail.com>
2021-01-26 03:54:28 +0900

    redesign URI

--- routes/api/index.ts
+++ routes/api/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import survey from './survey/index';
-import upload from './upload';
 
 const router = express.Router();
 
 router.use('/survey', survey);
-router.use('/upload', upload);
 
 export default router;
--- routes/api/survey/problem.ts
+++ routes/api/survey/problem.ts
@@ -1,9 +1,35 @@
 import express, { Request, Response } from 'express';
+import multer from 'multer';
+import path from 'path';
+import uuid from 'uuid';
 import db from '../../../models';
 import tool from '../tool';
 import form from '../formObj'
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req: Request, file: Express.Multer.File, cb: Function) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req: Request, file: Express.Multer.File, cb: Function) {
+        cb(null, req.newFileName + path.extname(file.originalname));
+    }
+});
+
+function fileFilter(res: Request, file: Express.Multer.File, cb: Function) {
+    const extension: string = file.mimetype.split('/')[0];
+
+    if (extension === 'image') //Check && Upload...
+        return cb(null, true);
+
+    return cb(new Error('업로드를 지원하지 않는 형식의 파일입니다.'), false);
+}
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter
+});
 
 router.put('/:problemId', async function (req: Request, res: Response) {
     const problemId: string = req.params.problemId;
@@ -181,7 +207,7 @@ router.put('/:problemId/choice/:choiceNum', async function (req: Request, res: R
     if (content) update['choice.$.content'] = content;
     if (updateNum) update['choice.$.choiceNum'] = updateNum;
     if (!content && !choiceNum) return res.status(400).send('content 혹은 choiceNum을 입력해 주십시오.');
-    
+
     try {
         const problem = await db.Problem.findOne({
             problemId,
@@ -200,7 +226,7 @@ router.put('/:problemId/choice/:choiceNum', async function (req: Request, res: R
                 'choice.choiceNum': updateNum,
                 'choice.isDeleted': false
             });
-    
+
             if (dup)
                 return res.status(403).send('이미 동일한 번호의 문항이 존재합니다.');
         }
@@ -244,6 +270,69 @@ router.delete('/:problemId/choice/:choiceNum', async function (req: Request, res
 
         res.sendStatus(200);
     } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.post('/:problemId/choice/:choiceNum/upload',
+    function (req: Request, file: object, next: Function) {
+        req.newFileName = uuid.v4();
+        next();
+    },
+    upload.single('attachment'),
+    async function (req: Request, res: Response) {
+        const problemId: string = req.params.problemId;
+        const choiceNum: string = req.params.choiceNum;
+        const file = req.file;
+
+        const ext = file.originalname.split('.');
+        const filename = req.newFileName + '.' + ext[ext.length - 1];
+
+        try {
+            const picture = await db.Problem.findOne({
+                'id': problemId,
+                'isDeleted': false,
+                'choice.choiceNum': choiceNum,
+                'choice.isDeleted': false
+            });
+
+            if (!picture)
+                return res.status(404).send('해당하는 질문 혹은 문항이 존재하지 않습니다.');
+
+            await db.Problem.updateOne({
+                'id': problemId,
+                'choice.choiceNum': choiceNum,
+                'choice.isDeleted': false
+            }, {
+                $set: {
+                    'choice.$.image': filename
+                }
+            });
+
+            res.sendStatus(200);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+);
+
+router.get('/:problemId/choice/:choiceNum/pic', async function (req: Request, res: Response) {
+    const problemId = req.params.problemId;
+    const choiceNum = req.params.choiceNum;
+
+    try {
+        const problem: any = await db.Problem.findOne({
+            'id': problemId,
+            'choice.choiceNum': choiceNum
+        });
+
+        if (!problem) return res.status(404).send('존재하지 않는 문제입니다.');
+        if (!problem.choice) return res.status(404).send('선택지가 존재하지 않습니다.');
+        if (!problem.choice[0].image) return res.status(404).send('이미지가 존재하지 않습니다.');
+
+        res.download('./uploads/' + problem.choice[0].image, problem.choice[0].image);
+    } catch (err) {
+        console.log(err);
         res.status(500).send(err);
     }
 });
