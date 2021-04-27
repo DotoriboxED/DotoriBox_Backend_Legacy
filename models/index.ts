488dfebbbc59d1f46b622687a8e3cbdaa8f2c8e1
Song <songyw9812@gmail.com>
2021-04-27 14:58:34 +0900

    fix problem ip error in WSL

--- models/index.ts
+++ models/index.ts
@@ -8,13 +8,30 @@ import surveySchema from './schema/survey';
 import userSchema from './schema/user';
 import itemSchema from './schema/sample';
 import productSchema from './schema/product';
+import dns from 'dns';
+import isWsl from 'is-wsl';
 
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
+if (isWsl) {
+    mongoose.connect('mongodb://' + dns.getServers()[0] +':27017/' + process.env.MONGO_DBNAME, 
+    {
+        auth: {
+            user: process.env.MONGO_USERNAME as string,
+            password: process.env.MONGO_PASSWORD as string
+        }, 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    })
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.log(err));
+} else {
+    mongoose.connect(process.env.MONGO_URI as string, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.log(err));
+}
 // mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI as string, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+
 mongoose.set('useCreateIndex', true);
 autoIncrement.initialize(mongoose.connection);
 
